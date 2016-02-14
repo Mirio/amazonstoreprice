63ffdd7bd2ac1a1768f32f820ea6097d106de94e
Alessandro <a.sbarbati@miriodev.net>
2016-02-14 02:03:46 +0100

    Adding license type on setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='amazonstoreprice',
     packages=['amazonstoreprice'],
     package_dir={'amazonestoreprice': 'amazonstoreprice'},
-    version='0.1',
+    version='0.1.1',
     install_requires=['requests', 'beautifulsoup4'],
     description='Find the price on Amazon store starting from url',
     author='Alessandro Sbarbati',
@@ -12,6 +12,7 @@ setup(
     url='https://github.com/Mirio/amazonstoreprice',
     download_url='https://github.com/Mirio/amazonstoreprice/tarball/0.1',
     keywords=['Amazon', 'amazonprice', 'amazonstoreprice', "amazonstore"],
+    license='BSD',
     classifiers=["License :: OSI Approved :: BSD License", "Programming Language :: Python :: 3",
                  "Topic :: Software Development :: Libraries :: Python Modules", "Topic :: Utilities"],
 )
